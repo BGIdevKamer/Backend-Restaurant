235b1ffbeabd4b854218a824dc0d0d934ff2798a
Abdel <djibrilbegni@example.com>
2025-01-16 17:47:35 +0100

    Inicialisation du projet

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models\User;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('login', function () {
+    $user = User::firstOrFail(); // Récupérer le premier utilisateur ou échouer
+    return $user->createToken('auth_token')->plainTextToken; // Créer un token pour cet utilisateur
+})->name('login');
